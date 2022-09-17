497e1c348c102959fa29baa0c28f6b3aeeffc7cb
Nabaraj <>
2022-09-17 12:19:37 +0545

    add add 1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-   <h2>Hwllow nabaraj</h2>
-<h4>kkaa</h4>
-<h5>hgjhghj</h5>
+        <h3>add 1</h3>
       </header>
     </div>
   );
