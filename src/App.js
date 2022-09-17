89fcec04541e1e11afe1de5b467aeea30d6f0588
Nabaraj <>
2022-09-17 13:19:18 +0545

    fix merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h3>add 1</h3>
         <h4>add 2</h4>
-        <h4>add 3</h4>
+        <h4>Add 2</h4>
       </header>
     </div>
   );
